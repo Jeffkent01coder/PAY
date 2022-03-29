04c88a6c8319e85d630bfd9f8f3ca22b693fc5bb
jeff <geoffreyerastus956@gmail.com>
2022-03-29 19:09:34 -0400

    initial

--- app/src/main/java/com/example/er_pay/ui/Settings.kt
+++ app/src/main/java/com/example/er_pay/ui/Settings.kt
@@ -1,7 +1,9 @@
 package com.example.er_pay.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.er_pay.databinding.ActivitySettingsBinding
 
 class Settings : AppCompatActivity() {
@@ -11,5 +13,18 @@ class Settings : AppCompatActivity() {
         supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.btncancel.setOnClickListener {
+            startActivity(Intent(this, Home::class.java))
+        }
+
+        binding.btnchange.setOnClickListener {
+            startActivity(Intent(this, Home::class.java))
+            Toast.makeText(this,"Successfully Made Changes!!!",Toast.LENGTH_LONG).show()
+        }
+
+        binding.ivBack.setOnClickListener {
+            startActivity(Intent(this, Home::class.java))
+        }
     }
 }
